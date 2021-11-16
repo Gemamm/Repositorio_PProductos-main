e054156ffaa4a6ff2fe4fe8e6ad2514efb936b5b
Gemamm <91058451+Gemamm@users.noreply.github.com>
2021-11-16 20:24:23 +0100

    Initial commit

---
+++ Proyecto_Producto/src/com/ceep/tienda/excepciones/ExcepcionesEscritura.java
@@ -0,0 +1,11 @@
+
+package com.ceep.tienda.excepciones;
+
+
+public class ExcepcionesEscritura  extends ExcepcionesAccesoDatos{
+
+    public ExcepcionesEscritura(String mensaje) {
+        super(mensaje);
+    }
+    
+}
---
+++ Proyecto_Producto/src/com/ceep/tienda/excepciones/ExcepcionesLectura.java
@@ -0,0 +1,9 @@
+package com.ceep.tienda.excepciones;
+
+public class ExcepcionesLectura extends ExcepcionesAccesoDatos {
+
+    public ExcepcionesLectura(String message) {
+        super(message);
+    }
+
+}
